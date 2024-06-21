6d8ce5b5cf2f4bb9b544f2e9a93618791eec1664
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-22 01:02:56 +0300

    add Greet and mention commands

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import os
-import dotenv
+import dotenv # type: ignore THIS IS TO FIX PYLANCE ON MY MACHINE
 import discord
+from discord import app_commands
+from discord.ext import commands
 
 def main():
-    client = discord.Client(intents=discord.Intents.all())
+    bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-    @client.event
-    async def on_message(message):
-        if message.author != client.user:
-            await message.channel.send("HEY!")
+    @bot.event
+    async def on_ready():
+        print("BOT IS RUNNING")
+        try:
+            synced = await bot.tree.sync()
+            print(f"Synced {len(synced)} command")
+        except Exception as e:
+            print(e)
+
+    @bot.tree.command(name="greet")
+    async def greet(interaction: discord.Interaction):
+        await interaction.response.send_message(f"Scream My Soldier {interaction.user.mention}", ephemeral=True)
+
+    @bot.tree.command(name="mention")
+    @app_commands.describe(who = "who")
+    async def mention(interaction: discord.Interaction, who: str):
+        await interaction.response.send_message(f"Hey Soldier{who}")
 
     dotenv.load_dotenv()
-    client.run(os.getenv("API_TOKEN"))
+    bot.run(os.getenv("API_TOKEN"))
+
 
 if __name__ == "__main__":
     main()
