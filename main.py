35729a31ee14f06b053073117b30eef3d44df3a6
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-21 19:36:18 +0300

    add working main

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 import dotenv
-
+import discord
 
 def main():
+    client = discord.Client(intents=discord.Intents.all())
+
+    @client.event
+    async def on_message(message):
+        if message.author != client.user:
+            await message.channel.send("HEY!")
+
     dotenv.load_dotenv()
-    print(os.getenv("API_TOKEN"))
+    client.run(os.getenv("API_TOKEN"))
 
 if __name__ == "__main__":
     main()
