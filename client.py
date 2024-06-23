c2f1472fb8b38e3bebf5b728cf5c6a02ffbf17d8
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-23 18:37:24 +0300

    added cogs and separate controllers, add load cogs function

--- client.py
+++ client.py
@@ -5,22 +5,29 @@ import dotenv
 import os
 import database
 
-from models.subscriber import Subscriber
-from models.task import Task
-from models.week import Week
-from models.penalty import Penalty
-
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+# loading cogs to sync other files commands
+async def load_cogs():
+    for filename in os.listdir('./controllers'):
+        if filename.endswith('.py'):
+            try:
+                await bot.load_extension(f'controllers.{filename[:-3]}')
+                print(f'Loaded extension: {filename}')
+            except Exception as e:
+                print(f'Failed to load extension {filename}: {e}')
+
+# ------------------------ GENERAL commands and events ------------------------
 
 @bot.event
 async def on_ready():
     print("BOT IS RUNNING")
+    await load_cogs()
     try:
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} command")
     except Exception as e:
         print(e)
-
 
 # Greet Command
 @bot.tree.command(name="greet")
@@ -34,33 +41,6 @@ async def greet(interaction: discord.Interaction):
 async def mention(interaction: discord.Interaction, who: str):
     await interaction.response.send_message(f"Hey Soldier{who}")
 
-# TODO: Add Tasks
-
-
-# Show Tasks
-@bot.tree.command(name="tasks")
-@app_commands.describe(who="who")
-async def show_tasks(interaction: discord.Interaction, who: str, task_num: int):
-    pass
-
-@bot.tree.command(name="register")
-async def register(interaction: discord.Interaction):
-    user_id = interaction.user.id
-    guild_id = interaction.guild.id
-    new_subscriber = Subscriber(user_id, guild_id)
-    try:
-        database.subscribe_user(new_subscriber)
-        await interaction.response.send_message(f"Be Proud, you are hard worker, you subscribed to the program succesfully {interaction.user.mention}", ephemeral=True)
-    except Exception as e:
-        await interaction.response.send_message("You are Already Subscribed or Cannot Subscribe again as you are banned!", ephemeral=True)
-
-
-
-# @bot.tree.command(name="delete_task")
-# @app_commands.describe(who="who")
-# @app_commands.describe(task_num="task_num")
-# async def delete_task(interaction: discord.Interaction, who: str, task_num: int):
-#     await interaction.response.send_message(f"Hey Soldier {who}, You Deleted Task {task_num}")
 
 
 # TODO: Get Task Instructions
