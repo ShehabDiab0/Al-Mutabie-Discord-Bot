28c8ac315b4cbb76ff9724721567e128631a96f2
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-24 04:12:18 +0300

    add guild_id, fix typos

--- models/task.py
+++ models/task.py
@@ -1,6 +1,7 @@
 class Task:
-    def __init__(self, task_id: int, description: str, week_number: float, owner_id: int, completion_percentage: float = 0.0):
-        self.discription = description
+    def __init__(self, guild_id, owner_id: int, description: str, week_number: float, completion_percentage: float = 0.0):
+        self.description = description
         self.completion_percentage = completion_percentage
         self.week_number = week_number
-        self.ownder_id = owner_id
+        self.guild_id = guild_id
+        self.owner_id = owner_id
