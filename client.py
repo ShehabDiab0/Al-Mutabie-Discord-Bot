4cd3028ea982c7dfc39ae6d1bfbde6e235943fa9
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-24 03:57:03 +0300

    added scheduler with pause/resume command, added threads

--- client.py
+++ client.py
@@ -4,6 +4,9 @@ from discord import app_commands
 import dotenv # type: ignore
 import os
 import database
+import schedule
+import threading
+import time
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -45,7 +48,38 @@ async def mention(interaction: discord.Interaction, who: str):
 
 # TODO: Get Task Instructions
 
+
+# ------------------------ Scheduling functions/commands ------------------------
+scheduler_running = True
+
+def run_scheduler():
+    global scheduler_running
+    while True:
+        if scheduler_running:
+            schedule.run_pending()
+        time.sleep(1)
+
+@bot.tree.command(name="pause_scheduler")
+async def pause(interaction: discord.Interaction):
+    global scheduler_running
+    scheduler_running = False
+    await interaction.response.send_message("Scheduler paused")
+
+@bot.tree.command(name="resume_scheduler")
+async def resume(interaction: discord.Interaction):
+    global scheduler_running
+    scheduler_running = True
+    await interaction.response.send_message("Scheduler resumed")
+
+
+# running the bot
 def run_bot():
     database.init_db()
     dotenv.load_dotenv()
+    
+    scheduler_thread = threading.Thread(target=run_scheduler)
+    scheduler_thread.setDaemon(True)
+    scheduler_thread.start()
+
     bot.run(os.getenv("API_TOKEN"))
+    
