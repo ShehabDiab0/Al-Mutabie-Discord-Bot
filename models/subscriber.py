d2782fa6dbeba07fb0d4aceeeedf35e0846462d4
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-24 21:19:15 +0300

    add is_banned, remove is_activated, is_allowed_to_reg

--- models/subscriber.py
+++ models/subscriber.py
@@ -1,6 +1,5 @@
 class Subscriber:
-    def __init__(self, user_id: str, guild_id: str, is_activated: bool=True, is_allowed_to_register: bool=True):
+    def __init__(self, user_id: str, guild_id: str, is_banned: bool=False):
         self.user_id = user_id
         self.guild_id = guild_id
-        self.is_activated = is_activated
-        self.is_allowed_to_register = is_allowed_to_register
+        self.is_banned = is_banned
