8040d774e1f8e79a809c40a8b0fca72915266a68
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-27 13:17:37 +0300

    add is_registered_guild function

--- data_access/guilds_access.py
+++ data_access/guilds_access.py
@@ -1,6 +1,7 @@
 from database import connection
 from models.guild import Guild
 
+
 def add_guild(new_guild: Guild):
     cursor = connection.cursor()
 
@@ -10,3 +11,18 @@ def add_guild(new_guild: Guild):
     ''', (new_guild.guild_id, new_guild.reminder_channel_id, new_guild.allow_kicks, new_guild.reminder_day, new_guild.offset_days))
     connection.commit()
     cursor.close()
+
+
+def is_registered_guild(guild_id: str) -> bool:
+    cursor = connection.cursor()
+    cursor.execute(f'''SELECT 1
+                    From 
+                    Guilds 
+                    WHERE guild_id = ?''',
+                    (guild_id,))
+    is_registered = cursor.fetchone()
+    connection.commit()
+    cursor.close()
+    if is_registered is None:
+        return False
+    return True
