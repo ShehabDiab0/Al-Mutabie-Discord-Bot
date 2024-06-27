f7dac90f2e3d0aa723c6b4f5be8ea137fd025041
Shehab Diab <shehabgehaddiab@gmail.com>
2024-06-27 13:20:58 +0300

    bug fix registering: multiple users in same server

--- controllers/subscribers_commands.py
+++ controllers/subscribers_commands.py
@@ -8,7 +8,7 @@ from models.task import Task
 from models.week import Week
 from models.penalty import Penalty
 from models.guild import Guild
-from data_access.guilds_access import add_guild
+from data_access.guilds_access import add_guild, is_registered_guild
 from data_access.subscribers_access import *
 
 class SubscribersCog(commands.Cog):
@@ -24,13 +24,15 @@ class SubscribersCog(commands.Cog):
             await interaction.response.send_message('يا متخازل ------ Completion Threshold has to be >= 0.5')
             return
 
-        user_id = interaction.user.id
-        guild_id = interaction.guild.id
+        user_id = str(interaction.user.id)
+        guild_id = str(interaction.guild.id)
         new_subscriber = Subscriber(user_id, guild_id, default_yellow_description, default_red_description, task_completion_threshold)
         new_guild = Guild(guild_id)
 
+        if not is_registered_guild(guild_id):
+                add_guild(new_guild)
+
         try:
-            add_guild(new_guild)
             subscribe_user(new_subscriber)
             await interaction.response.send_message(
                 f"Be Proud, you are a hard worker, you subscribed to the program successfully {interaction.user.mention}",
@@ -42,7 +44,7 @@ class SubscribersCog(commands.Cog):
                 "You are already subscribed or cannot subscribe again as you are banned!",
                 ephemeral=True
             )
-    
+        
     # TODO: Unban user
 
     # TODO: Get Banned users
