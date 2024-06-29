e71115a8dcca22ccb3f83c9c06c0069f93d00bea
MohamedAmgad2002 <mohamedamgad233@gmail.com>
2024-06-29 15:14:21 +0300

    Update penalty

--- models/penalty.py
+++ models/penalty.py
@@ -1,8 +1,9 @@
 class Penalty:
-    def __init__(self,penalty_id:int, description: str, is_done: bool, is_yellow: bool, week_number: int, owner_id: int):
+    def __init__(self, guild_id:str, description: str, is_done: bool, is_yellow: bool, week_number: int, owner_id: int, penalty_id:int = None):
         self.penalty_id = penalty_id
+        self.guild_id = guild_id
         self.description = description
         self.is_done = is_done
         self.is_yellow = is_yellow
         self.week_number = week_number
-        self.owner = owner_id
+        self.owner_id = owner_id
