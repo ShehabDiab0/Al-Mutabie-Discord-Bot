547f7784bab8479379494da4345be4c5b7e36c76
MohamedAmgad2002 <mohamedamgad233@gmail.com>
2024-06-29 15:06:03 +0300

    Rename attribute

--- models/penalty.py
+++ models/penalty.py
@@ -1,6 +1,6 @@
 class Penalty:
-    def __init__(self,penality_id:int, description: str, is_done: bool, is_yellow: bool, week_number: int, owner_id: int):
-        self.penality_id = penality_id
+    def __init__(self,penalty_id:int, description: str, is_done: bool, is_yellow: bool, week_number: int, owner_id: int):
+        self.penalty_id = penalty_id
         self.description = description
         self.is_done = is_done
         self.is_yellow = is_yellow
