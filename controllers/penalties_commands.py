3c044aa94df6808b5c0a8785bcee876dd4571833
MohamedAmgad2002 <mohamedamgad233@gmail.com>
2024-06-29 19:29:30 +0300

    Added penalty done command

--- controllers/penalties_commands.py
+++ controllers/penalties_commands.py
@@ -1,71 +1,37 @@
+import discord
+from typing import Optional
 from discord.ext import commands
-# from client import kick, reminder
+from discord import app_commands
 from models.subscriber import Subscriber
 from models.task import Task
 from models.week import Week
 from models.penalty import Penalty
-from data_access import penalties_access, weeks_access, subscribers_access, guilds_access
+from data_access.subscribers_access import is_registered_user
+from data_access.penalties_access import get_penalty, update_subscriber_penalty
 class PenaltiesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    # Penalty Completed
+    @app_commands.command(name="penalty_done")
+    async def penalty_done(self, interaction: discord.Interaction):
+        task_owner_id: str = str(interaction.user.id)
+        guild_id: str = str(interaction.guild.id)
+        penalty = get_penalty(task_owner_id, guild_id)
 
-# TODO: reminders and guild scheduling
-
-    def run_penalties(self, day: int) -> None:
-        reminder_guilds, apply_guilds = guilds_access.get_today_guilds(day)
-        for guild in reminder_guilds:
-            self.weekly_check(guild.guild_id, True)
-        for guild in apply_guilds:
-            self.weekly_check(guild.guild_id, False)
-
-
-    def weekly_check(self, guild_id, remind: bool) -> None:
-        week_num = weeks_access.get_current_week()
-        # get all users having this guild_id and not is_banned in a list of ids
-        subscribers = []
-        for subscriber in subscribers:
-            previous_card = penalties_access.get_subscriber_penalty_history(subscriber=subscriber)
-            if previous_card:
-                previous_card = previous_card[-1]
-            else:
-                previous_card = None
-            card = self.check_user(subscriber, week_num, previous_card)
-            if card:
-                if remind:
-                    # reminder(subscriber.user_id, guild_id)
-                    return
-                is_yellow = 1
-                desc = subscriber.default_yellow_penalty_description
-                if previous_card:
-                    # red card
-                    is_yellow = 0
-                    # kick(subscriber.user_id, guild_id)
-                    desc = subscriber.default_red_penalty_description
-                    subscribers_access.ban_user(subscriber)
-                # add penalty in db
-                penalty = Penalty(description=desc, is_yellow=is_yellow, week_number=week_num, guild_id=guild_id, owner_id=subscriber.user_id)
-                penalties_access.add_penalty(penalty)
-
-
-    # checks user weekly progress and returns true if he should recieve a card
-    def check_user(self, subscriber: Subscriber, week_num: int, previous_card: Penalty) -> bool:
-        if previous_card and not previous_card.is_done:
-            return True
-        tasks = subscribers_access.get_subscriber_tasks(subscriber, week_num)
-        total = len(tasks)
-        if not total:
-            return True
-        completed = 0.0
-        for task in tasks:
-            completed += task.completion_percentage
-        return completed / total < subscriber.threshold
+        if not is_registered_user(task_owner_id, guild_id):
+            await interaction.response.send_message(f"You have to register first before adding any tasks please use /register to register")
+            return
+        
+        if penalty:
+            penalty.is_done = True
+            # update penalty
+            update_subscriber_penalty(penalty)
+            await interaction.response.send_message(f"Penalty marked as done, Congratulations", ephemeral=True)
+        else:
+            await interaction.response.send_message(f"No penalty to mark as done", ephemeral=True)
 
 
 
 async def setup(bot):
     await bot.add_cog(PenaltiesCog(bot))
-
-def run(day: int):
-    penalties = PenaltiesCog()
-    print("running penalties")
-    penalties.run_penalties(day)
