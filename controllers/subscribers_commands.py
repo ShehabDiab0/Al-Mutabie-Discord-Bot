d40887af74ba112d3fa87f49845ba23c42856d87
OmarElgammal1 <omarelgammal2004@gmail.com>
2024-06-29 17:04:41 +0300

    Returned code

--- controllers/subscribers_commands.py
+++ controllers/subscribers_commands.py
@@ -44,7 +44,64 @@ class SubscribersCog(commands.Cog):
                 "You are already subscribed or cannot subscribe again as you are banned!",
                 ephemeral=True
             )
+
+
+    @app_commands.command(name="update_default_threshold")
+    @app_commands.describe(new_threshold="enter the threshold percentage you want >= 0.5 (default is 0.6) (we apply a penalty if you completed less than the threshold percentage)")
+    async def update_default_threshold(self, interaction: discord.Interaction, new_threshold: Optional[float] = 0.6):
+        if new_threshold < 0.5:
+            await interaction.response.send_message('يا متخازل ------ Completion Threshold has to be >= 0.5')
+            return
         
+        user_id = str(interaction.user.id)
+        guild_id = str(interaction.guild.id)
+
+        if not is_registered_user(user_id, guild_id):
+            await interaction.response.send_message(
+                    'You are not registered please register using /register, (you can also choose threshold there)',
+                    ephemeral=True
+                )
+            return
+        
+        if is_banned_user(user_id, guild_id):
+            await interaction.response.send_message(
+                    'banned متخازل كبير'
+                )
+            return
+        
+        try:
+            update_subscriber_threshold(user_id, guild_id, new_threshold)
+            await interaction.response.send_message(f'your threshold is now {new_threshold}', ephemeral=True)
+        except Exception as e:
+            print(e)
+
+
+    @app_commands.command(name="update_default_yellow_penalty")
+    @app_commands.describe(default_yellow_description="Write your default Yellow card description.")
+    async def update_default_threshold(self, interaction: discord.Interaction, default_yellow_description: str):
+        user_id = str(interaction.user.id)
+        guild_id = str(interaction.guild.id)
+
+        if not is_registered_user(user_id, guild_id):
+            await interaction.response.send_message(
+                    'You are not registered please register using /register, (you can also choose yellow description there)',
+                    ephemeral=True
+                )
+            return
+        
+        if is_banned_user(user_id, guild_id):
+            await interaction.response.send_message(
+                    'banned متخازل كبير'
+                )
+            return
+        
+        try:
+            update_subscriber_yellow_card(user_id, guild_id, default_yellow_description)
+            await interaction.response.send_message(f'your new default for yellow is now {default_yellow_description}', ephemeral=True)
+        except Exception as e:
+            print(e)
+
+
     # TODO: Unban user
 
     # TODO: Get Banned users
