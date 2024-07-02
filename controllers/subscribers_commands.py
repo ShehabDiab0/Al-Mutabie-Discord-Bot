d50daaedadb68bd50b0cbc2992cf38fd2b27067d
Shehab Diab <shehabgehaddiab@gmail.com>
2024-07-03 00:58:57 +0300

    refactor register with modals

--- controllers/subscribers_commands.py
+++ controllers/subscribers_commands.py
@@ -10,40 +10,56 @@ from models.penalty import Penalty
 from models.guild import Guild
 from data_access.guilds_access import add_guild, is_registered_guild
 from data_access.subscribers_access import *
+import UI
 
 class SubscribersCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="register")
-    @app_commands.describe(default_yellow_description="Write your default Yellow card description.")
-    @app_commands.describe(default_red_description="Write your default Red card description.")
-    @app_commands.describe(task_completion_threshold="enter the threshold percentage you want >= 0.5 (default is 0.6) (we apply a penalty if you completed less than the threshold percentage)")
-    async def register(self, interaction: discord.Interaction, default_yellow_description: str, default_red_description: str, task_completion_threshold: float = 0.6):
-        if task_completion_threshold < 0.5:
-            await interaction.response.send_message('يا متخازل ------ Completion Threshold has to be >= 0.5')
-            return
-
+    async def register(self, interaction: discord.Interaction):
         user_id = str(interaction.user.id)
-        guild_id = str(interaction.guild.id)
-        new_subscriber = Subscriber(user_id, guild_id, default_yellow_description, default_red_description, task_completion_threshold)
+        guild_id = str(interaction.guild_id)
         new_guild = Guild(guild_id)
 
         if not is_registered_guild(guild_id):
                 add_guild(new_guild)
+        
+        if is_registered_user(user_id, guild_id):
+            await interaction.response.send_message(
+                "You are already subscribed or cannot subscribe again as you are banned! (you can edit profile using /edit_profile command)",
+                ephemeral=True
+            )
+            return
 
-        try:
-            subscribe_user(new_subscriber)
+        modal = UI.RegisterationModal()
+        await interaction.response.send_modal(modal)
+
+    @app_commands.command(name="show_profile")
+    @app_commands.describe(who="mention a user to know their tasks")
+    async def show_profile(self, interaction: discord.Interaction, who: Optional[str] = "-1"):
+        if who == "-1":
+            who = f'<@{interaction.user.id}>'
+
+        user_id = who[2:-1] # when u mention somebody in discord it uses the format <@user_id>
+        guild_id: str = str(interaction.guild.id)
+
+        if not is_registered_user(user_id, guild_id):
             await interaction.response.send_message(
-                f"Be Proud, you are a hard worker, you subscribed to the program successfully {interaction.user.mention}",
-                ephemeral=True
-            )
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message(
-                "You are already subscribed or cannot subscribe again as you are banned!",
-                ephemeral=True
-            )
+                    'You are not registered please register using /register',
+                    ephemeral=True
+                )
+            return
+
+        subscriber: Subscriber = get_subscriber(user_id, guild_id)
+        
+
+        
+        
+        
+    @app_commands.command(name="edit_profile")
+    async def edit_profile(self, interaction: discord.Interaction):
+        pass
 
 
     @app_commands.command(name="update_default_threshold")
