67694ddff6107e086949dcf1a149c21eed74380d
Shehab Diab <shehabgehaddiab@gmail.com>
2024-07-11 04:10:10 +0300

    remove default val from reminder_channel_id

--- models/guild.py
+++ models/guild.py
@@ -1,5 +1,5 @@
 class Guild:
-    def __init__(self, guild_id: str, reminder_channel_id: str = "", allow_kicks: bool = False, reminder_day: int = 0, offset_days: int = 2):
+    def __init__(self, guild_id: str, reminder_channel_id: str, allow_kicks: bool = False, reminder_day: int = 0, offset_days: int = 2):
         self.guild_id = guild_id
         self.reminder_channel_id = reminder_channel_id
         self.allow_kicks = allow_kicks
