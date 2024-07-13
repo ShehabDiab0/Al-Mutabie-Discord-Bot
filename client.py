ca7e3bf1981f45a4f89f9b7ca26fbf1e488fadc3
OmarElgammal1 <omarelgammal2004@gmail.com>
2024-07-13 16:36:09 +0300

    Cancel APScheduler

--- client.py
+++ client.py
@@ -29,7 +29,7 @@ DAY_TO_RESET = "thu"
 
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-scheduler = AsyncIOScheduler()
+# scheduler = AsyncIOScheduler()
 
 
 # loading cogs to sync other files commands
@@ -181,33 +181,33 @@ async def kick(user_id: str, guild_id: str):
 
 # ------------------------ Scheduling functions/commands ------------------------
 
-async def run_scheduler():
-    scheduler.start()
-    scheduler.add_job(weeks_access.add_week, trigger='cron', day_of_week=DAY_TO_RESET, timezone=TIMEZONE)
-    # scheduler.add_job(weeks_access.add_week, trigger='cron',second='*', timezone=TIMEZONE)
-    #print all the jobs
-    print("Scheduler started")
+# async def run_scheduler():
+#     scheduler.start()
+#     scheduler.add_job(weeks_access.add_week, trigger='cron', day_of_week=DAY_TO_RESET, timezone=TIMEZONE)
+#     # scheduler.add_job(weeks_access.add_week, trigger='cron',second='*', timezone=TIMEZONE)
+#     #print all the jobs
+#     print("Scheduler started")
 
-@bot.tree.command(name="pause_scheduler")
-@app_commands.checks.has_permissions(administrator=True)
-@commands.guild_only()
-async def pause_scheduler(interaction: discord.Interaction):
-    scheduler.pause()
-    await interaction.response.send_message("Scheduler paused")
+# @bot.tree.command(name="pause_scheduler")
+# @app_commands.checks.has_permissions(administrator=True)
+# @commands.guild_only()
+# async def pause_scheduler(interaction: discord.Interaction):
+#     scheduler.pause()
+#     await interaction.response.send_message("Scheduler paused")
 
-@bot.tree.command(name="resume_scheduler")
-@app_commands.checks.has_permissions(administrator=True)
-@commands.guild_only()
-async def resume_scheduler(interaction: discord.Interaction):
-    scheduler.resume()
-    await interaction.response.send_message("Scheduler resumed")
+# @bot.tree.command(name="resume_scheduler")
+# @app_commands.checks.has_permissions(administrator=True)
+# @commands.guild_only()
+# async def resume_scheduler(interaction: discord.Interaction):
+#     scheduler.resume()
+#     await interaction.response.send_message("Scheduler resumed")
 
 
-@pause_scheduler.error
-@resume_scheduler.error
-async def scheduler_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
-    if isinstance(error, app_commands.errors.MissingPermissions):
-        await interaction.response.send_message("You don't have permissions to use this command.", ephemeral=True)
+# @pause_scheduler.error
+# @resume_scheduler.error
+# async def scheduler_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+#     if isinstance(error, app_commands.errors.MissingPermissions):
+#         await interaction.response.send_message("You don't have permissions to use this command.", ephemeral=True)
 
 #----------------------------penalties-------------------------------
     
