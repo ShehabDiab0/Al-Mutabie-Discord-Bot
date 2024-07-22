aaa4dd1b7332d5b1f0030e00e048d126a862a442
OmarElgammal1 <omarelgammal2004@gmail.com>
2024-07-22 12:52:16 +0300

    Get penalties for week

--- data_access/penalties_access.py
+++ data_access/penalties_access.py
@@ -55,3 +55,19 @@ def get_penalty(user_id: str, guild_id: str) -> Penalty:
     if penalty:
         return Penalty(penalty_id=penalty[0], description=penalty[1], is_done=penalty[2], is_yellow=penalty[3], week_number=penalty[4], guild_id=guild_id, owner_id=user_id)
     return None
+
+
+def get_penalties_for_week(week_number: int, guild_id: str) -> list[Penalty]:
+    cursor = connection.cursor()
+    cursor.execute(f"""
+                    SELECT penalty_id, description, is_done, is_yellow, week_number, global_user_id
+                    FROM Penalties
+                    WHERE week_number = ? AND guild_id = ?
+                    """, (week_number, guild_id))
+    output = cursor.fetchall()
+    penalties = []
+    for penalty in output:
+        penalties.append(Penalty(penalty_id=penalty[0], description=penalty[1], is_done=penalty[2], is_yellow=penalty[3], week_number=penalty[4], guild_id=guild_id, owner_id=penalty[5]))
+    connection.commit()
+    cursor.close()
+    return penalties
