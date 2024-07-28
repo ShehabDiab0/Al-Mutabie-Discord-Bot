5d9115d8f6f8bcd40874cdc0e34303588f1ad9e0
Shehab Diab <shehabgehaddiab@gmail.com>
2024-07-28 16:24:19 +0300

    modify help and add more commands instructions

--- client.py
+++ client.py
@@ -289,21 +289,30 @@ async def instructions(interaction: discord.Interaction):
 **Commands Description:**
 1. /register: To register to the bot
 2. To show someone Profile or your Profile use /show_profile, Parameters: who
-3. To edit your profile use /edit_profile
+3. To edit your profile use /edit_profile (you can edit threshold, default yellow card, default red card)
 4. To add a task use /add_single_task, The tasks should be 1 to 100 chars each
 5. To add multiple tasks use /add_multiple_tasks, Please seperate them by dashes, The tasks should be 1 to 100 chars each
 6. To show Someone or your tasks use /show_tasks, Parameters: who, week_number
-7. To delete a task use /delete_task
-8. To update a task use /update_task, Parameters: week_number
-9. To report your week progress use /self_report, Parameters: week_number
-10. To mark your penalty as done use /finished_penalty
+7. To show tasks for all subscribers use /show_tasks_for_all, parameters: week_number
+8. To delete multiple tasks use /delete_task
+9. To update a single task use /update_task, Parameters: week_number
+10. To quickly update your week progress or tasks completion percentages use /self_report, Parameters: week_number
+11. To mark your penalty as done use /finished_penalty
+12. to mention this week's users who received a yellow/red card this week use /mention_penalized_users
+13. to mention subscribed users use /mention_subscribers (does not mention banned users)
+14. to get subscriber's penalty history use /penalty_history
+15. [Server Admin Only] To allow kicks in your server use /allow_kicks (You still have to grant the bot permissions to kick members)
+16. [Server Admin Only] To disallow kicks in your server use /disallow_kicks
+17. [Server Admin Only] to change reminder channel use /set_reminder_channel (by default reminder channel is the server default channel) (use this by going to the channel u want to set and use this command)
+18. [Server Admin Only] to reset reminder channel to server default user /reset_reminder_channel
+19. [Server Admin Only] to unban a user to be able to use the bot again use /unban_user, Parameters: who
 **Rules:**
 • Week starts & Reminders ==> Friday 00:00
 • Penalties ==> Sunday 00:00
 • You get a penalty in 3 conditions (yellow then red):
 ------>1. you did not complete enough tasks to pass the threshold percentage you registered with
 ------>2. you did not write your tasks on time
------->3. you did not complete your previous penalty 
+------>3. you did not complete your previous penalty (You have to mark it as done)
 • you get a red card if you have 1 penalty previous week and you recieved a new one
 • Warning: getting a red card would ban you from using the bot
 • You get kicked once getting a red card if the server allows kicks from the bot
