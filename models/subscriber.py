ac71921512e38e380eb198f978e2118000285ad6
SuperNova74-k <kingkhaled.w@gmail.com>
2024-08-01 15:04:24 +0300

    feat/strictmode

--- models/subscriber.py
+++ models/subscriber.py
@@ -1,8 +1,9 @@
 class Subscriber:
-    def __init__(self, user_id: str, guild_id: str, default_yellow_description: str = "", default_red_description: str = "", threshold_percentage: float = 60.0, is_banned: bool=False):
+    def __init__(self, user_id: str, guild_id: str, default_yellow_description: str = "", default_red_description: str = "", threshold_percentage: float = 60.0, strict_mode: bool = True, is_banned: bool=False):
         self.user_id = user_id
         self.guild_id = guild_id
         self.default_yellow_description = default_yellow_description
         self.default_red_description = default_red_description
         self.threshold_percentage = threshold_percentage
+        self.strict_mode = strict_mode
         self.is_banned = is_banned
