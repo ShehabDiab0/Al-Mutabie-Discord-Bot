2654f2280deac8073c4ff7a0b7bf54ff40bfbb0c
SuperNova74-k <kingkhaled.w@gmail.com>
2024-08-01 15:05:03 +0300

    refactor/pep8

--- models/subscriber.py
+++ models/subscriber.py
@@ -1,5 +1,7 @@
 class Subscriber:
-    def __init__(self, user_id: str, guild_id: str, default_yellow_description: str = "", default_red_description: str = "", threshold_percentage: float = 60.0, strict_mode: bool = True, is_banned: bool=False):
+    def __init__(self, user_id: str, guild_id: str, default_yellow_description: str = "",
+                 default_red_description: str = "", threshold_percentage: float = 60.0, strict_mode: bool = True,
+                 is_banned: bool = False):
         self.user_id = user_id
         self.guild_id = guild_id
         self.default_yellow_description = default_yellow_description
